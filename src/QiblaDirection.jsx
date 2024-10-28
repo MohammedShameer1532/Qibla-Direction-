a2a5f1e3d46afbf371bafe796699d5344e9ca9d4
Mohammed Shameer <118889168+MohammedShameer1532@users.noreply.github.com>
2024-10-28 15:57:14 +0530

    Update QiblaDirection.jsx

--- src/QiblaDirection.jsx
+++ src/QiblaDirection.jsx
@@ -9,7 +9,7 @@ const QiblaDirection = ({ latitude, longitude }) => {
     // Fetch Qibla direction based on user location
     const fetchQibla = async () => {
       try {
-        const res = await axios.get(`http://api.aladhan.com/v1/qibla/${latitude}/${longitude}`);
+        const res = await axios.get(`https://api.aladhan.com/v1/qibla/${latitude}/${longitude}`);
         const qiblaDirection = res?.data?.data?.direction;
         setDirection(qiblaDirection);
       } catch (error) {
